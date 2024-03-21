f6681aae4c5968b71a9eca0cf7faac313c90203b
fedeval98 <fede.val9806@gmail.com>
2024-03-21 19:47:06 -0300

    arreglando redes

--- js.js
+++ js.js
@@ -11,13 +11,9 @@ window.onload = function () {
     const boton = event.target
   
     if (boton.classList.contains('facebook')) {
-      window.open("https://www.facebook.com/fede.val.9")
+      window.open("https://www.facebook.com/profile.php?id=100089264902183")
     } else if (boton.classList.contains('instagram')) {
       window.open("https://www.instagram.com/opythatv/")
-    } else if (boton.classList.contains('github')) {
-      window.open("https://github.com/fedeval98")
-    } else if (boton.classList.contains('linkedin')) {
-      window.open("https://www.linkedin.com/in/federico-val")
     } else if (boton.classList.contains('twitter')) {
       window.open("https://twitter.com/_Opytha")
     } else if (boton.classList.contains('twitch')) {
